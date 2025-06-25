777793cab3cff3b9ffbe2e7188cdd602c0aadeb9
Renan Freitas <renanfsouza135@gmail.com>
2025-06-25 20:36:01 -0300

    Update logica.js

--- portifolio/pro07/logica.js
+++ portifolio/pro07/logica.js
@@ -2,10 +2,12 @@ function processar() {
     let subtotal = parseFloat(document.getElementById('subtotal').value);
     let percentual = parseFloat(document.getElementById('desconto').value);
     let recebido = parseFloat(document.getElementById('recebido').value);
+    let troco = document.getElementById('troco');
     let total_venda = document.getElementById('total');
 
-
     total_venda.value = subtotal * (1 + percentual / 100);
+    total_venda.toLocaleString('pt-br', {style: 'currency'})
+    troco.value = recebido - parseFloat(total_venda.value);
     
 }
 
