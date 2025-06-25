59ebe23f2cd71c42a3ca325e61e51018f807d043
Renan Freitas <renanfsouza135@gmail.com>
2025-06-25 20:16:13 -0300

    atualizacao do projeto

--- portifolio/pro07/logica.js
+++ portifolio/pro07/logica.js
@@ -1,3 +1,12 @@
-function continuar() {
+function processar() {
+    let subtotal = parseFloat(document.getElementById('subtotal').value);
+    let percentual = parseFloat(document.getElementById('desconto').value);
+    let recebido = parseFloat(document.getElementById('recebido').value);
+    let total_venda = document.getElementById('total');
 
+
+    total_venda.value = subtotal * (1 + percentual / 100);
+    
 }
+
+processar()
