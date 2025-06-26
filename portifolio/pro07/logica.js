47c0301bba2c11158a73ef0f6088d2acf3298794
Renan Freitas <renanfsouza135@gmail.com>
2025-06-26 19:36:13 -0300

    update

--- portifolio/pro07/logica.js
+++ portifolio/pro07/logica.js
@@ -1,18 +1,24 @@
+
 function processar() {
-    validar()
+    /* validar() */
     let subtotal = parseFloat(document.getElementById('subtotal').value);
     let percentual = parseFloat(document.getElementById('desconto').value);
     let recebido = parseFloat(document.getElementById('recebido').value);
     let troco = document.getElementById('troco');
     let total_venda = document.getElementById('total');
-
-    total_venda.value = subtotal * (1 + percentual / 100);
-    total_venda.toLocaleString('pt-br', {style: 'currency'})
-    troco.value = recebido - parseFloat(total_venda.value);
-
+    
+    let total_calculado = subtotal * (1 - percentual / 100);
+    total_venda.value = total_calculado.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+    troco.value = (recebido - total_calculado).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+    
 }
 
-function validar() {
+document.getElementById('formulario').addEventListener('submit', function (e) {
+    // Se o formulário for inválido, ele não chama isso aqui
+    e.preventDefault(); // impede o envio, mas valida antes
+    processar();
+});
+/* function validar() {
     let subtotal = document.getElementById('subtotal');
     let percentual = document.getElementById('desconto');
     let recebido = document.getElementById('recebido');
@@ -25,5 +31,6 @@ function validar() {
     }
 }
 
-/* 
+
+
 processar() */
