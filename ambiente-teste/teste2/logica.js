57cd4dbed529be8ec21383971c8662fa2c3cd7db
Renan Freitas <renanfsouza135@gmail.com>
2025-07-30 11:33:17 -0300

    up

--- ambiente-teste/teste2/logica.js
+++ ambiente-teste/teste2/logica.js
@@ -2,8 +2,9 @@ function adicionarTarefa() {
     let input = document.getElementById('inputTarefa').value;
     let lista = document.getElementById('lista')
     let item = document.createElement('li')
+    let msg = document.querySelector('p')
+    msg.remove()
     item.textContent = input
-
     lista.appendChild(item)
 
 
